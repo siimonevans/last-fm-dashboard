39a4cbdc8e6539b2b7e1e2b9244aaebfcd8d3128
Simon Evans <simon.evans@torchbox.com>
2017-04-30 19:28:36 +0100

    Add ‘Now Playing’

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -4,9 +4,9 @@ import UserInfo from '../../components/UserInfo/UserInfo'
 import Scrobbles from '../../components/Scrobbles/Scrobbles'
 import TopTracks from '../../components/TopTracks/TopTracks'
 import TopArtists from '../../components/TopArtists/TopArtists'
-import TopAlbums from '../../components/TopAlbums/TopAlbums'
 import RecentTracks from '../../components/RecentTracks/RecentTracks'
 import MostPlayed from '../../components/MostPlayed/MostPlayed'
+import NowPlaying from '../../components/NowPlaying/NowPlaying'
 
 class Grid extends Component {
   render() {
@@ -14,10 +14,10 @@ class Grid extends Component {
       <div className="wrapper">
         <ul className="grid">
           <li><UserInfo /></li>
-          <li><TopArtists /></li>
+          <li><NowPlaying /></li>
           <li><MostPlayed /></li>
           <li><Scrobbles /></li>
-          <li><TopAlbums /></li>
+          <li><TopArtists /></li>
           <li><TopTracks /></li>
           <li><RecentTracks /></li>
         </ul>
---
+++ src/components/NowPlaying/NowPlaying.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import './NowPlaying.css'
+
+class NowPlaying extends Component {
+
+  getNowPlaying() {
+
+    let key      = '168bcdd68b93bc37ed26d3670c203322',
+        userName = 'TheSimonEvans',
+        apiLink  = 'http://ws.audioscrobbler.com/2.0/';
+
+    fetch(apiLink +'?method=user.getRecentTracks&user='+ userName +'&api_key='+ key +'&format=json&limit=1')
+        .then( response => response.json() )
+        .then(function(data) {
+
+          let nowPlaying = data.recenttracks.track[0]
+          let name   = nowPlaying.name;
+          let artist = nowPlaying.artist['#text'];
+
+          const titleHTML = document.querySelector('.now-playing__title')
+          const artistHTML = document.querySelector('.now-playing__artist')
+
+          titleHTML.innerHTML += name
+          artistHTML.innerHTML += artist
+
+      })
+  }
+
+  componentDidMount() {
+    this.getNowPlaying()
+  }
+
+  render() {
+    return (
+      <div className="now-playing">
+        <div className="grid__heading grid__heading--white">Now Playing</div>
+        <div className="now-playing__inner">
+          <div className="now-playing__title"></div>
+          <div className="now-playing__artist"></div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default NowPlaying
